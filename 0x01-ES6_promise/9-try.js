e299f71d7a3e6dc5edcb9655b96c6d4c2ded68fa
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2024-04-13 07:45:27 +0200

    Promises-t#9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,13 +1,13 @@
 export default function guardrail(mathFunction) {
-  const queue = []; // Create the queue
+  const queue = [];
   
   try {
     const result = mathFunction();
-    queue.push(result); // Add the function's result
+    queue.push(result);
   } catch (error) {
-    queue.push(error.message); // Add the error message
+    queue.push(`Error: ${error.message}`);
   } finally {
-    queue.push("Guardrail was processed"); // Always add this message
+    queue.push("Guardrail was processed");
   }
   
   return queue;
