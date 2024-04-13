43c40f27db8f33cc5ce5576780a7fb2358f58b1b
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2024-04-13 06:33:36 +0200

    indenting

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,12 @@
 export default function getFullResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
-        if (success) {
-            resolve({
-                body: 'Success',
-                status: 200
-            });
-        } else {
-            reject(new Error('The fake API is not working currently'));
-        }
-    });
+  return new Promise((resolve, reject) => {
+      if (success) {
+          resolve({
+              body: 'Success',
+              status: 200
+          });
+      } else {
+          reject(new Error('The fake API is not working currently'));
+      }
+  });
 }
