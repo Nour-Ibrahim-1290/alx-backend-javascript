b8812057b71caa5999b8cb79aa61dd0db2c7f66d
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2024-04-13 06:12:44 +0200

    Promises-t#1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,10 @@
+export default function getFullResponseFromAPI(success) {
+    return new Promise((resolve, reject) => {
+      if (success) {
+        resolve({body: 'Success',
+status: 200});
+      } else {
+        reject(new Error('The fake API is not working currently'));
+      }
+    });
+  }
