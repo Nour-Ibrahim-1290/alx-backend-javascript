f788c37f3f01a1ab58512688843b8b0c05185f1e
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2024-09-19 07:50:45 +0300

    fix-nodejs-task#2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,30 +2,30 @@
 const fs = require('fs').promises;
 
 function countStudents(path) {
-  return  fs.readFile(path, 'utf8')
+  return fs.readFile(path, 'utf8')
     .then((data) => {
-        const lines = data.split('\n').filter((line) => line.trim() !== '');
-        const students = lines.slice(1);
-        const totalStudents = students.length;
-        console.log(`Number of students: ${totalStudents}`);
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const students = lines.slice(1);
+      const totalStudents = students.length;
+      console.log(`Number of students: ${totalStudents}`);
 
-        const fields = {};
-        students.forEach((student) => {
-            const [firstname, , , field] = student.split(',').map((item) => item.trim());
-            if (field) {
-                if (!fields[field]) {
-                fields[field] = [];
-                }
-                fields[field].push(firstname);
-            }
-        });
-
-        for (const [field, names] of Object.entries(fields)) {
-        console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+      const fields = {};
+      students.forEach((student) => {
+        const [firstname, , , field] = student.split(',').map((item) => item.trim());
+        if (field) {
+          if (!fields[field]) {
+            fields[field] = [];
+          }
+          fields[field].push(firstname);
         }
+      });
+
+      for (const [field, names] of Object.entries(fields)) {
+        console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+      }
     })
     .catch(() => {
-        throw new Error('Cannot load the database');
+      throw new Error('Cannot load the database');
     });
 }
 
