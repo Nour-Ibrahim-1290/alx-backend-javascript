c66514ad6e8c631a18e5baba982295854500785b
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2024-09-21 05:34:00 +0300

    unittests-task#1

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,16 @@
+// this function returns the sum of 2 numbers
+
+function calculateNumber(type, a, b) {
+    if (type === 'SUM') {
+        return Math.round(a) + Math.round(b);
+      }
+      if (type === 'SUBTRACT') {
+        return Math.round(a) - Math.round(b);
+      }
+      if (type === 'DIVIDE') {
+        return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
+      }
+      return 0;
+}
+  
+module.exports = calculateNumber;
